b07f62ad77d18a038c54ec630532f4dc8dba3877
Björn Birath <bjorn.birath@gmail.com>
2018-02-03 01:31:14 +0100

    Started work on deploying to heroku

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='calendar-filterer',
+    packages=['app'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'icalendar',
+        'requests',
+        'google-api-python-client',
+        'google-auth',
+        'google-auth-oauthlib',
+        'google-auth-httplib2'
+    ]
+)
