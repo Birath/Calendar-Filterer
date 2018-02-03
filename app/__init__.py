d02d5110a340980e9394146145874d4a81d758fc
Björn Birath <bjorn.birath@gmail.com>
2018-02-03 18:12:06 +0100

    Moved secret files

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('config.DevelopmentConfig')
-
+    app.config.from_pyfile('config.py', silent=True)
     from app.blueprints import calendar_filterer
     from app.blueprints import oauth
     app.register_blueprint(calendar_filterer.bp)
--- run.py
+++ run.py
@@ -4,5 +4,4 @@ import ssl
 app = create_app()
 context = ssl.SSLContext()
 context.load_cert_chain("secrets/cert.pem", "secrets/key.pem")
-app.config.from_object('config.DevelopmentConfig')
 app.run(ssl_context=context)
